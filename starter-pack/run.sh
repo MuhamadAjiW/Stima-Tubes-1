e27d294b1a4a8157913d82b4e9697e24164dc9d0
MuhamadAjiW <16521119@mahasiswa.itb.ac.id>
2023-02-07 01:00:57 +0700

    feat: added first iteration bot

--- starter-pack/run.sh
+++ starter-pack/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Bot sparring:
+# Lv0: cd ./reference-bot-publish/ && sleep 3 && dotnet ReferenceBot.dll &
+# Lv1: cd ./dummyBots/Dummy1/ && sleep 3 && java -jar Dummy1.jar &
 
 cd ./runner-publish/ && dotnet GameRunner.dll &
 cd ./engine-publish/ && sleep 1 && dotnet Engine.dll &
@@ -6,6 +9,6 @@ cd ./logger-publish/ && sleep 1 && dotnet Logger.dll &
 cd ./reference-bot-publish/ && sleep 3 && dotnet ReferenceBot.dll &
 cd ./reference-bot-publish/ && sleep 3 && dotnet ReferenceBot.dll &
 cd ./reference-bot-publish/ && sleep 3 && dotnet ReferenceBot.dll &
-cd ./starter-bots/JavaBot/target && sleep 3 && java -jar JavaBot.jar &
+cd ./dummyBots/Dummy1/ && sleep 3 && java -jar Dummy1.jar &
 
 wait
