cab04d51f66ae989a0e4ccb9a2e6587f3cedce04
MuhamadAjiW <16521119@mahasiswa.itb.ac.id>
2023-02-08 17:35:00 +0700

    feat: performance tweaks

--- starter-pack/starter-bots/JavaBot/src/main/java/Services/Common/Trajectory.java
+++ starter-pack/starter-bots/JavaBot/src/main/java/Services/Common/Trajectory.java
@@ -7,12 +7,16 @@ public class Trajectory {
     public long gradient;
     public long constant;
     public long vel;
+    public double vy;
+    public double vx;
     public int tetha;
     public Position init = new Position();
 
     public Trajectory(GameObject object){
         tetha = object.currentHeading;
         vel = object.getSpeed();
+        vx = Math.cos(Math.toRadians(tetha))*vel;
+        vy = Math.sin(Math.toRadians(tetha))*vel;
         init.x = object.getPosition().x;
         init.y = object.getPosition().y;
         gradient = Math.round(Math.sin(Math.toRadians(tetha)) * vel);
@@ -29,8 +33,8 @@ public class Trajectory {
 
     public Position interpolate(int time){
         Position retval = new Position();
-        retval.x = (int) (Math.round(Math.cos(Math.toRadians(tetha))*vel)*time + init.x);
-        retval.y = (int) (Math.round(Math.sin(Math.toRadians(tetha))*vel)*time + init.y);
+        retval.x = (int) Math.round(vx*time + init.x);
+        retval.y = (int) Math.round(vy*time + init.y);
         return retval;
     }
 }
